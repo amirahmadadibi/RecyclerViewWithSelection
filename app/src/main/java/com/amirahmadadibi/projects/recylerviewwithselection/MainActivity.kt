4253c3f596720577c9250293c703508c3cec1e1b
amirahmadadibi <amirahmadadibi@gmail.com>
2019-11-15 18:24:17 +0330

    compelte basic reyclerview

--- app/src/main/java/com/amirahmadadibi/projects/recylerviewwithselection/MainActivity.kt
+++ app/src/main/java/com/amirahmadadibi/projects/recylerviewwithselection/MainActivity.kt
@@ -2,12 +2,20 @@ package com.amirahmadadibi.projects.recylerviewwithselection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.amirahmadadibi.projects.recylerviewwithselection.model.User
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var userList: List<User> = listOf(User("test1"), User("test2"), User("test"),
+            User("test1"), User("test2"), User("test"),User("test1"), User("test2"), User("test"),
+            User("test1"), User("test2"), User("test"))
+        recyclerview_user_userlist.adapter = MyAdapter(this, userList)
+        recyclerview_user_userlist.layoutManager = LinearLayoutManager(this)
 
     }
 }
--- app/src/main/java/com/amirahmadadibi/projects/recylerviewwithselection/MyAdapter.kt
+++ app/src/main/java/com/amirahmadadibi/projects/recylerviewwithselection/MyAdapter.kt
@@ -1,32 +1,34 @@
 package com.amirahmadadibi.projects.recylerviewwithselection
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.amirahmadadibi.projects.recylerviewwithselection.model.User
+import kotlinx.android.synthetic.main.list_item.view.*
 
 /**
  * Created by Amirahmad Adibi.
  * RecylerViewWithSelection | Copyrights 2019-11-15.
  */
-class MyAdapter(context:Context,userList:List<User>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-
+class MyAdapter(var context: Context, var userList: List<User>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        var view: View = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(view)
     }
 
     override fun getItemCount(): Int {
+        return userList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.text_view_username.text = userList.get(position).username
     }
 
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        init {
-
-        }
-    }
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }

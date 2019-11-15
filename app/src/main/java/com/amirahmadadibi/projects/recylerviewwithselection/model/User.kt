6ae0e27981e37a2d4fd764633cb5e5388032f155
amirahmadadibi <amirahmadadibi@gmail.com>
2019-11-15 16:28:37 +0330

    adding dependency

---
+++ app/src/main/java/com/amirahmadadibi/projects/recylerviewwithselection/model/User.kt
@@ -0,0 +1,7 @@
+package com.amirahmadadibi.projects.recylerviewwithselection.model
+
+/**
+ * Created by Amirahmad Adibi.
+ * RecylerViewWithSelection | Copyrights 2019-11-15.
+ */
+data class User(var username: String)
